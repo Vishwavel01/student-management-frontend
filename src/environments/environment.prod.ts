82dc5e7c40635b5ed281f6e5c16c8ca01e317f78
Vishwavel01 <vishwavelmurugan01@gmail.com>
2026-03-14 16:18:12 +0530

    Api URL Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://student-management-backend-production-2b73.up.railway.app/StuManageSys'
+  apiUrl: 'https://student-management-backend-production-d7cc.up.railway.app/StuManageSys'
 };
