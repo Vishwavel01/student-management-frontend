1e3684521514b93a8698b6bb93a010f27d2151e1
Vishwavel01 <vishwavelmurugan01@gmail.com>
2026-03-14 12:13:10 +0530

    debugging

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Student, ApiResponse } from '../models/student.model';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment.prod'; 
 
 @Injectable({
   providedIn: 'root'
