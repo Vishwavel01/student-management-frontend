20d8da36b32f2c420771bc916c306b355f2c8c68
Vishwavel01 <vishwavelmurugan01@gmail.com>
2026-04-02 07:35:53 +0530

    Update API URL for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://student-management-backend-production-d7cc.up.railway.app/StuManageSys'
-  apiUrl: 'http://localhost:8080/StuManageSys'
+  apiUrl: 'https://student-management-backend-production-d7cc.up.railway.app/StuManageSys'
 };
