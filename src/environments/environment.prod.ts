08ac6f75dfdd9e6dd18b22bae82274d8e56d9a24
Vishwavel01 <vishwavelmurugan01@gmail.com>
2026-04-02 07:28:29 +0530

    Change apiUrl to localhost for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://student-management-backend-production-d7cc.up.railway.app/StuManageSys'
+  //apiUrl: 'https://student-management-backend-production-d7cc.up.railway.app/StuManageSys'
+  apiUrl: 'http://localhost:8080/StuManageSys'
 };
