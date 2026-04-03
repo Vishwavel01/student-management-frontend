9d9249088229877caef205c49b90289ae7253622
Vishwavel01 <vishwavelmurugan01@gmail.com>
2026-04-03 10:43:14 +0530

    Update apiUrl to include StuManageSys path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
  // apiUrl: 'http://localhost:8080/StuManageSys'
-  apiUrl: "https://studentmanagementsystem-oxno.onrender.com"
+  apiUrl: "https://studentmanagementsystem-oxno.onrender.com/StuManageSys"
 };
