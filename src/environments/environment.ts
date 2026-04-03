911b5a1f03d37241b364af3295aa7c12482251f1
Vishwavel01 <vishwavelmurugan01@gmail.com>
2026-04-03 10:42:39 +0530

    Update API URL to production endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/StuManageSys'
+ // apiUrl: 'http://localhost:8080/StuManageSys'
+  apiUrl: "https://studentmanagementsystem-oxno.onrender.com"
 };
